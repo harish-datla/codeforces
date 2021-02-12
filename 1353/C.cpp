e34fda9776ff46f913abd620cd866ff3a730d94c
harish-datla <harishkumardatla@gmail.com>
2021-02-13 00:28:45 +0530

    Add solution for 1353C

---
+++ 1353/C.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		long long n;
+        cin >> n;
+        long long y = n/2;
+        cout << 8*(y)*(y+1)*(2*y+1)/6 << endl;
+        
+ 
+	}
+ 
+}
