5d08aefc4c626589e80b2103ca041989bebc7e27
harish-datla <harishkumardatla@gmail.com>
2021-02-13 00:25:33 +0530

    Add solution for 1409A

---
+++ 1409/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        cout << abs(a-b)/10  + (abs(a-b)%10 != 0) << endl;
+    }
+}
